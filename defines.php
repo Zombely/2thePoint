b7f0278e6a565ac6cbdd629f9342bb634894e5e6
Zombely <51829060+Zombely@users.noreply.github.com>
2020-02-20 20:32:29 +0100

    Create defines.php

---
+++ defines.php
@@ -0,0 +1,4 @@
+<?php
+	define( 'INSTAGRAM_APP_ID', '497074931001105' );
+	define( 'INSTAGRAM_APP_SECRET', '07dca48948f01f775e87308b6dd4cf6d' );
+	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://zombely.github.io/2thePoint/' );
